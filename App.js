7d9526c0cfc9bbc804d6b8c18500b7144d87a60f
Ashley Beck <ab733266@southeast.edu>
2022-10-28 20:45:35 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, ScrollView, StyleSheet, SafeAreaView, } from 'react-native';
+import PostContainer from './PostContainer';
+import PhotoViewer from './PhotoViewer';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const greatWallOfChina = require('./images/wall.png');
+const petra = require('./images/petra.png');
+const theRedeemer = require('./images/redeemer.png');
+const machuPicchu = require('./images/machu.png');
+const chichenItza = require('./images/chichen.png');
+const colosseum = require('./images/colosseum.png');
+const tajMahal = require('./images/taj.png');
+
+const timeline = [
+  { title: 'Great Wall of China', image: greatWallOfChina },
+  { title: 'Petra', image: petra },
+  { title: 'The Redeemer', image:  theRedeemer },
+  { title: 'Machu Picchu', image: machuPicchu },
+  { title: 'Chichen Itza', image: chichenItza },
+  { title: 'Colosseum', image: colosseum },
+  { title: 'Taj Mahal', image: tajMahal },
+];
+
+export default class App extends Component {
+  state = {
+    selected: null,
+    position: null,
+  };
+
+  showImage = (selected, position) => {
+    this.setState({
+      selected,
+      position,
+    });
+  }
+
+  closeViewer = () => {
+    this.setState({
+      selected: null,
+      position: null,
+    });
+  }
+
+  renderViewer() {
+    const { selected, position } = this.state;
+
+    if (selected) {
+      return (
+        <PhotoViewer
+          post={selected}
+          position={position}
+          onClose={this.closeViewer}
+        />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.main}>
+        <Text style={styles.toolbar}>Seven Wonders of the World</Text>
+        <ScrollView style={styles.content}>
+        {
+          timeline.map((post, index) =>
+            <PostContainer key={index} post={post}
+            onPress={this.showImage} />
+          )
+        }
+        </ScrollView>
+        {this.renderViewer()}
+      </SafeAreaView>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
+    backgroundColor: '#ecf0f1',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  toolbar: {
+    backgroundColor: '#2c3e50',
+    color: '#fff',
+    fontSize: 22,
+    padding: 20,
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
   },
 });
